cec5d3bf7fde37d065448dbb33ba2aec3ebe2e59
Kyle Anderson <Kyle@divspace.com>
2020-05-21 12:10:20 -0700

    Update migrations

--- database/migrations/2020_04_25_000000_create_users_table.php
+++ database/migrations/2020_04_25_000000_create_users_table.php
@@ -6,30 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->efficientUuid('uuid')->index();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('sobriety_date')->nullable();
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
---
+++ database/migrations/2020_04_26_000000_create_programs_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->efficientUuid('uuid')->index();
+            $table->tinyInteger('program_type_id')->unsigned();
+            $table->string('name')->unique();
+            $table->string('abbreviation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('programs');
+    }
+}
