ae9b9a883b8737b10136691045d965a95c2a5d1d
Kyle Anderson <Kyle@divspace.com>
2020-05-21 14:06:02 -0700

    Add user and program routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
+Route::get('users', 'UserController@index');
+Route::get('users/{user}', 'UserController@show');
+
+Route::get('programs', 'ProgramController@index');
+Route::get('programs/{program}', 'ProgramController@show');
+
 Route::middleware('guest')->group(function () {
     Route::view('login', 'auth.login')->name('login');
     Route::view('register', 'auth.register')->name('register');
