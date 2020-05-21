f57c4524efa4055c8685da751d5fe5898d006d96
Kyle Anderson <Kyle@divspace.com>
2020-05-21 12:47:25 -0700

    Add user and program controller

---
+++ app/Http/Controllers/ProgramController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Program;
+use Illuminate\Http\Request;
+
+class ProgramController extends Controller
+{
+    public function index(): \Illuminate\View\View
+    {
+        $programs = Program::orderBy('name')->get();
+
+        return view('programs.show', compact('programs'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Program $program): Program
+    {
+        return $program;
+    }
+
+    public function edit(Program $program)
+    {
+        //
+    }
+
+    public function update(Request $request, Program $program)
+    {
+        //
+    }
+
+    public function destroy(Program $program)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(User $user): \Illuminate\View\View
+    {
+        $soberDate = Carbon::parse($user->sobriety_date);
+        $soberDays = $soberDate->diffInDays().' '.Str::plural('day', $soberDate->diffInDays());
+
+        return view('users.show', compact('user', 'soberDate', 'soberDays'));
+    }
+
+    public function edit(User $user)
+    {
+        //
+    }
+
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    public function destroy(User $user)
+    {
+        //
+    }
+}
