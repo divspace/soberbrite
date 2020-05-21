f6f7c43921ccd99b6fe6e0a7213fa0ce1ec7291a
Kyle Anderson <Kyle@divspace.com>
2020-05-21 12:48:11 -0700

    Update namespace

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Program;
+use App\Database\Models\Program;
 use Illuminate\Http\Request;
 
 class ProgramController extends Controller
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Database\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
