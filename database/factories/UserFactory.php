57a4aa0abbce4f7061d76cde713a46817b330cd7
Kyle Anderson <Kyle@divspace.com>
2020-05-22 14:27:49 -0700

    Hash the password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,12 +2,13 @@
 
 use App\Database\Models\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
         'email' => $faker->unique()->safeEmail,
-        'password' => '$argon2id$v=19$m=1024,t=2,p=2$M0F1Y0VFeUoza2VFRW9GUg$VdtmnRfhQ/ZupTEXpK43lymW6jTWU0GrmTdDwJI4t8Q', // password
+        'password' => Hash::make('password'),
         'remember_token' => Str::random(10),
         'email_verified_at' => now(),
     ];
