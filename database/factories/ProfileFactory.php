9603b2cd7f51f64bfa42fd421f87e93ac14a0ff5
Kyle Anderson <Kyle@divspace.com>
2020-05-22 14:27:22 -0700

    Use better Faker data

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -4,11 +4,20 @@ use App\Database\Models\Profile;
 use Faker\Generator as Faker;
 
 $factory->define(Profile::class, function (Faker $faker) {
+    $sex = rand(1, 100) <= 75 ? 'male' : 'female';;
+
+    $phoneNumber = $faker->randomDigitNot(0);
+
+    for ($i = 0; $i < 9; ++$i) {
+        $phoneNumber .= $faker->randomDigit;
+    }
+
     return [
-        'user_id' => 1,
         'username' => $faker->unique()->userName,
-        'first_name' => $faker->firstName,
+        'first_name' => $faker->firstName($sex),
+        'middle_name' => $faker->optional(25)->firstName($sex),
         'last_name' => $faker->lastName,
-        'sobriety_date' => $faker->date('Y-m-d', 'now'),
+        'phone' => $phoneNumber,
+        'sobriety_date' => $faker->dateTimeBetween('-30 years', 'now')->format('Y-m-d'),
     ];
 });
