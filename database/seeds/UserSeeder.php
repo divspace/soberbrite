8f03e009b5be428c24363100104b1d50d3eceffa
Kyle Anderson <Kyle@divspace.com>
2020-05-22 14:28:54 -0700

    Create master user and 50 fake users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Database\Models\Profile;
 use App\Database\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -7,8 +8,19 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        factory(User::class)->create([
-            'email' => 'kyle@soberbrite.com',
+        factory(Profile::class)->create([
+            'user_id' => factory(User::class)->create([
+                'email' => 'kyle@soberbrite.com',
+            ])->id,
+            'username' => 'soberbrite',
+            'first_name' => 'Kyle',
+            'middle_name' => null,
+            'last_name' => 'Anderson',
+            'sobriety_date' => '2020-03-15',
         ]);
+
+        factory(User::class, 50)->create()->each(function ($user) {
+            $user->profile()->save(factory(Profile::class)->make());
+        });
     }
 }
