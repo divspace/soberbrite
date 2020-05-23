0c53c65e2882e90725833651d0e844416e7d78d2
Kyle Anderson <Kyle@divspace.com>
2020-05-22 22:55:35 -0700

    Add param type hints

--- app/Database/Casts/Uuid.php
+++ app/Database/Casts/Uuid.php
@@ -6,12 +6,12 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class Uuid implements CastsAttributes
 {
-    public function get($model, string $key, $value, array $attributes)
+    public function get($model, string $key, $value, array $attributes): string
     {
         return $model->resolveUuid()->fromBytes($value)->toString();
     }
 
-    public function set($model, string $key, $value, array $attributes)
+    public function set($model, string $key, $value, array $attributes): array
     {
         return [
             $key => $model->resolveUuid()->fromString(strtolower($value))->getBytes(),
