1666e9535e39eccf97f73cf348af3f5b9a6ce993
Kyle Anderson <Kyle@divspace.com>
2020-05-22 23:18:31 -0700

    Display address and phone number

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -50,7 +50,35 @@
                     </dt>
 
                     <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                        {{ $user->profile->sobriety_date->format('F j, Y') }} ({{ $user->profile->sobriety_date->diffInDays().' '.Str::plural('day', $user->profile->sobriety_date->diffInDays()) }})
+                        {{ $user->profile->sobriety_date->format('F j, Y') }} ({{ number_format($user->profile->sobriety_date->diffInDays()).' '.Str::plural('day', $user->profile->sobriety_date->diffInDays()) }})
+                    </dd>
+                </div>
+
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm leading-5 font-medium text-gray-500">
+                        Phone Number
+                    </dt>
+
+                    <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                        {{ '('.substr($user->profile->phone, 0, 3).') '.substr($user->profile->phone, 3, 3).'-'.substr($user->profile->phone,6) }}
+                    </dd>
+                </div>
+
+                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm leading-5 font-medium text-gray-500">
+                        Address
+                    </dt>
+
+                    <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                        {{ $user->address->first()->line_1 }}
+
+                        @isset($user->address->first()->line_2)
+                            {{ $user->address->first()->line_2 }}
+                        @endisset
+
+                        <br>
+
+                        {{ $user->address->first()->city }}, {{ $user->address->first()->state }} {{ $user->address->first()->zip_code }}
                     </dd>
                 </div>
 
