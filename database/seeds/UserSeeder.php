ea07661710a447d4bf861a5db5158c913e392c01
Kyle Anderson <Kyle@divspace.com>
2020-05-22 22:56:51 -0700

    Generate address for users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Database\Models\Address;
 use App\Database\Models\Profile;
 use App\Database\Models\User;
 use Illuminate\Database\Seeder;
@@ -8,10 +9,12 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        $primaryUser = factory(User::class)->create([
+            'email' => 'kyle@soberbrite.com',
+        ]);
+
         factory(Profile::class)->create([
-            'user_id' => factory(User::class)->create([
-                'email' => 'kyle@soberbrite.com',
-            ])->id,
+            'user_id' => $primaryUser->id,
             'username' => 'soberbrite',
             'first_name' => 'Kyle',
             'middle_name' => null,
@@ -19,7 +22,17 @@ class UserSeeder extends Seeder
             'sobriety_date' => '2020-03-15',
         ]);
 
+        factory(Address::class)->create([
+            'user_id' => $primaryUser->id,
+            'line_1' => '3300 N Scottsdale Rd',
+            'line_2' => null,
+            'city' => 'Scottsdale',
+            'state' => 'AZ',
+            'zip_code' => '85251',
+        ]);
+
         factory(User::class, 50)->create()->each(function ($user) {
+            $user->address()->save(factory(Address::class)->make());
             $user->profile()->save(factory(Profile::class)->make());
         });
     }
