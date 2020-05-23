972e2e95e068aa466e22c9f69f6ddd8b2c38a09c
Kyle Anderson <Kyle@divspace.com>
2020-05-22 22:58:04 -0700

    Remove custom cast

--- app/Database/Models/Address.php
+++ app/Database/Models/Address.php
@@ -2,7 +2,6 @@
 
 namespace App\Database\Models;
 
-use App\Database\Casts\Address as AddressCast;
 use App\Database\Models\User;
 use App\Database\Traits\HasTimestamps;
 use App\Database\Traits\HasUuid;
