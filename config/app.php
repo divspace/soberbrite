5e2f75178aa38997cc1a5d422edfa42f541d707e
Kyle Anderson <Kyle@divspace.com>
2020-05-22 21:03:45 -0700

    Remove UUID service provider

--- config/app.php
+++ config/app.php
@@ -45,7 +45,6 @@ return [
         /*
 -        * Package Service Providers...
 -        */
-        // Dyrynda\Database\LaravelEfficientUuidServiceProvider::class,
 
         /*
 -        * Application Service Providers...
