097bcfe2d0b23b863d93e1cbcc03c3c9e9a06a3a
Kyle Anderson <Kyle@divspace.com>
2020-05-24 01:03:17 -0700

    Check if tables are empty before seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,13 +1,22 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(ZipCodeSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(ProgramSeeder::class);
+        if (!DB::table('zip_codes')->count()) {
+            $this->call(ZipCodeSeeder::class);
+        }
+
+       if (!DB::table('programs')->count()) {
+            $this->call(ProgramSeeder::class);
+        }
+
+        if (!DB::table('users')->count()) {
+            $this->call(UserSeeder::class);
+        }
     }
 }
