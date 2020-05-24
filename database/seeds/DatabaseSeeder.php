b068f6f27d576af5494dc4e1ae622fbdcbba20b8
Kyle Anderson <Kyle@divspace.com>
2020-05-23 23:48:26 -0700

    Call ZIP Code seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->call(ZipCodeSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(ProgramSeeder::class);
     }
