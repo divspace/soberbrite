a9719efa60c182d2ca277f712e33c43cda7161a3
Kyle Anderson <Kyle@divspace.com>
2020-05-26 16:03:13 -0700

    Fetch state ID by code

--- database/seeds/LocationSeeder.php
+++ database/seeds/LocationSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Database\Models\Location;
+use App\Database\Models\State;
 use Illuminate\Database\Seeder;
 use Grimzy\LaravelMysqlSpatial\Types\Point;
 
@@ -15,21 +16,23 @@ class LocationSeeder extends Seeder
 
         while (($row = fgetcsv($file, 100, ';')) !== false) {
             $city = $row[1];
-            $state = $row[2];
+            $state = State::whereCode($row[2])->first();
             $zipCode = $row[0];
             $latitude = $row[3];
             $longitude = $row[4];
             $timezoneOffset = $row[5];
             $observesDst = $row[6];
 
-            factory(Location::class)->create([
-                'zip_code' => $zipCode,
-                'city' => $city,
-                'state' => $state,
-                'coordinate' => new Point($latitude, $longitude),
-                'timezone_offset' => $timezoneOffset.':00:00',
-                'observes_dst' => $observesDst,
-            ]);
+            if ($state) {
+                factory(Location::class)->create([
+                    'state_id' => $state->id,
+                    'zip_code' => $zipCode,
+                    'city' => $city,
+                    'coordinate' => new Point($latitude, $longitude),
+                    'timezone_offset' => $timezoneOffset.':00:00',
+                    'observes_dst' => $observesDst,
+                ]);
+            }
         }
 
         fclose($file);
