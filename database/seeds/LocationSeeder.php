a1bac45efe1ce21774b84e4edfa433bd93a05a8f
Kyle Anderson <Kyle@divspace.com>
2020-05-26 14:12:55 -0700

    Do nothing with the first line

--- database/seeds/LocationSeeder.php
+++ database/seeds/LocationSeeder.php
@@ -9,23 +9,29 @@ class LocationSeeder extends Seeder
     public function run(): void
     {
         /** @see https://public.opendatasoft.com */
-        $csv = fopen(storage_path('csv/zip-codes.csv'), 'r');
+        $file = fopen(storage_path('csv/zip-codes.csv'), 'r');
 
-        while (($row = fgetcsv($csv, 100, ';')) !== false) {
-            if (!is_numeric($row[0])) {
-                continue;
-            }
+        fgetcsv($file, 100, ';');
+
+        while (($row = fgetcsv($file, 100, ';')) !== false) {
+            $city = $row[1];
+            $state = $row[2];
+            $zipCode = $row[0];
+            $latitude = $row[3];
+            $longitude = $row[4];
+            $timezoneOffset = $row[5];
+            $observesDst = $row[6];
 
             factory(Location::class)->create([
-                'zip_code' => $row[0],
-                'city' => $row[1],
-                'state' => $row[2],
-                'coordinate' => new Point($row[3], $row[4]),
-                'timezone_offset' => $row[5].':00:00',
-                'observes_dst' => $row[6],
+                'zip_code' => $zipCode,
+                'city' => $city,
+                'state' => $state,
+                'coordinate' => new Point($latitude, $longitude),
+                'timezone_offset' => $timezoneOffset.':00:00',
+                'observes_dst' => $observesDst,
             ]);
         }
 
-        fclose($csv);
+        fclose($file);
     }
 }
