8cda732293b074ce87066fc89ed45960147ef446
Kyle Anderson <Kyle@divspace.com>
2020-05-26 16:02:51 -0700

    Use state's ID instead of code

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -1,14 +1,15 @@
 <?php
 
 use App\Database\Models\Location;
+use App\Database\Models\State;
 use Faker\Generator as Faker;
 use Grimzy\LaravelMysqlSpatial\Types\Point;
 
 $factory->define(Location::class, function (Faker $faker) {
     return [
+        'state_id' => fn () => factory(State::class)->create()->id,
         'zip_code' => substr($faker->postcode, 0, 5),
         'city' => $faker->city,
-        'state' => $faker->stateAbbr,
         'coordinate' => new Point($faker->latitude, $faker->longitude),
         'timezone_offset' => rand(-10, -4).':00:00',
         'observes_dst' => rand(0, 1),
