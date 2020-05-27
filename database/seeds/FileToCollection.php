182c0f4a0766dd8c733d9a3a89951ea4d2d572b4
Kyle Anderson <Kyle@divspace.com>
2020-05-27 13:09:19 -0700

    Add method to get states

--- database/seeds/FileToCollection.php
+++ database/seeds/FileToCollection.php
@@ -14,13 +14,33 @@ class FileToCollection
 
         while (($row = fgetcsv($file, 100, ';')) !== false) {
             $data->push([
-                'city' => $row[1],
-                'stateCode' => $row[1],
+                'cityName' => $row[1],
+                'stateCode' => $row[2],
                 'zipCode' => $row[0],
                 'latitude' => (float) $row[3],
                 'longitude' => (float) $row[4],
                 'timezoneOffset' => (int) $row[5],
                 'observesDst' => (bool) $row[6],
+            ]);
+        }
+
+        fclose($file);
+
+        return $data;
+    }
+
+    public function getStates(): Collection
+    {
+        $data = collect();
+
+        $file = fopen(storage_path('csv/states.csv'), 'r');
+
+        fgetcsv($file, 25, ';');
+
+        while (($row = fgetcsv($file, 25, ';')) !== false) {
+            $data->push([
+                'code' => $row[0],
+                'name' => $row[1],
             ]);
         }
 
