0d29ab58e5a030cd6d2d387cb7a948daf25bda80
Kyle Anderson <Kyle@divspace.com>
2020-05-27 21:37:38 -0700

    Use shorthand instead of alias

--- database/migrations/2020_04_24_000000_create_failed_jobs_table.php
+++ database/migrations/2020_04_24_000000_create_failed_jobs_table.php
@@ -9,7 +9,7 @@ class CreateFailedJobsTable extends Migration
     public function up(): void
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
