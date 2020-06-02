f01a2d8faa01f57c28c367307949423d95487ef4
Kyle Anderson <Kyle@divspace.com>
2020-06-01 23:51:32 -0700

    Refactor

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,40 +1,28 @@
 <?php
 
-use App\Database\Models\Location;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
+    private $tables = [
+        'cities',
+        'states',
+        'zip_codes',
+        'area_codes',
+        'countries',
+        'locations',
+        'programs',
+        'users',
+    ];
+
     public function run(): void
     {
-        if (!DB::table('cities')->count()) {
-            $this->call(CitySeeder::class);
-        }
-
-        if (!DB::table('states')->count()) {
-            $this->call(StateSeeder::class);
-        }
-
-        if (!DB::table('zip_codes')->count()) {
-            $this->call(ZipCodeSeeder::class);
-        }
-
-        if (!DB::table('countries')->count()) {
-            $this->call(CountrySeeder::class);
-        }
-
-        if (!DB::table('locations')->count()) {
-            $this->call(LocationSeeder::class);
-        }
-
-       if (!DB::table('programs')->count()) {
-            $this->call(ProgramSeeder::class);
-        }
-
-        if (!DB::table('users')->count()) {
-            $this->call(UserSeeder::class);
-        }
+        collect($this->tables)->each(function ($table) {
+            if (!DB::table($table)->count()) {
+                $this->call(Str::of($table)->studly()->singular().'Seeder'::class);
+            }
+        });
     }
 }
