5362ffd71100e9ac96ab7c038967fd1d0116321c
Kyle Anderson <Kyle@divspace.com>
2020-06-10 19:33:32 -0700

    Push data to collection

--- database/seeds/LocationSeeder.php
+++ database/seeds/LocationSeeder.php
@@ -10,28 +10,26 @@ final class LocationSeeder extends LookupSeeder
 {
     public function run(): void
     {
-        $this->insertData = (new Lookup('locations'))
-            ->get()
-            ->transform(function (array $location): ?array {
+        (new Lookup('locations'))
+            ->fetch()
+            ->each(function (array $location): void {
                 $city = City::where('name', $location['cityName'])->first();
                 $state = State::where('code', $location['stateCode'])->first();
                 $zipCode = ZipCode::where('code', $location['zipCode'])->first();
 
                 if (isset($city, $state, $zipCode)) {
-                    return [
+                    $this->insertData->push([
                         Location::CITY => $city->id,
                         Location::STATE => $state->id,
                         Location::ZIP_CODE => $zipCode->id,
-                        Location::LATITUDE => $location['latitude'],
-                        Location::LONGITUDE => $location['longitude'],
+                        Location::LATITUDE => $location[Location::LATITUDE],
+                        Location::LONGITUDE => $location[Location::LONGITUDE],
                         Location::TIMEZONE_OFFSET => $location['timezoneOffset'].':00:00',
                         Location::OBSERVES_DST => $location['observesDst'],
                         Location::CREATED_AT => $this->timestamp,
                         Location::UPDATED_AT => $this->timestamp,
-                    ];
+                    ]);
                 }
-
-                return null;
             });
 
         $this->insertInChunks(Location::TABLE);
