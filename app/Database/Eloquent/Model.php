b5405e8512b906aa135970b76111cf3653816f78
Kyle Anderson <Kyle@divspace.com>
2020-06-10 17:28:55 -0700

    Add constant for ID

--- app/Database/Eloquent/Model.php
+++ app/Database/Eloquent/Model.php
@@ -9,83 +9,54 @@ class Model extends BaseModel
 {
     use HasTimestamps;
 
-    /**
-     * @var string
-     */
+    /** @var string */
+    public const ID = 'id';
+
+    /** @var string */
     public const CREATED_AT = 'created_at';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const DELETED_AT = 'deleted_at';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const UPDATED_AT = 'updated_at';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const ARRAY = 'array';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const BOOLEAN = 'boolean';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const COLLECTION = 'collection';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const DATE = 'date';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const DATETIME = 'datetime';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const DECIMAL = 'decimal';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const DOUBLE = 'double';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const FLOAT = 'float';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const INTEGER = 'integer';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const OBJECT = 'object';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const REAL = 'real';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const STRING = 'string';
 
-    /**
-     * @var string
-     */
+    /** @var string */
     public const TIMESTAMP = 'timestamp';
 }
