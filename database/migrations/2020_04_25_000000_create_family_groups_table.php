63db0785176892e48ee539ab043c5fc71efa58e4
Kyle Anderson <Kyle@divspace.com>
2020-06-12 13:38:19 -0700

    Create `family_groups` table

--- database/migrations/2020_04_25_000000_create_family_groups_table.php
+++ database/migrations/2020_04_25_000000_create_family_groups_table.php
@@ -4,23 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProgramsTable extends Migration
+class CreateFamilyGroupsTable extends Migration
 {
     public function up(): void
     {
-        Schema::create('programs', static function (Blueprint $table): void {
+        Schema::create('family_groups', static function (Blueprint $table): void {
             $table->tinyIncrements('id');
-            $table->tinyInteger('program_type_id')->unsigned();
             $table->string('name')->unique();
             $table->string('abbreviation')->nullable();
             $table->timestamps();
-
-            $table->foreign('program_type_id')->references('id')->on('program_types');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('programs');
+        Schema::dropIfExists('family_groups');
     }
 }
