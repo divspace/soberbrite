b66a5cec288ef6f44eb5428bdb6962e38354d50f
Kyle Anderson <Kyle@divspace.com>
2020-06-12 13:44:14 -0700

    Remove constants

--- config/lookup.php
+++ config/lookup.php
@@ -1,41 +1,37 @@
 <?php
 
-const FILE_PATH = 'file_path';
-
-const FILL_ORDER = 'fill_order';
-
 return [
 
     'tables' => [
 
         'area_codes' => [
-            FILL_ORDER => 6,
-            FILE_PATH => storage_path('csv/area-codes.csv'),
+            'fill_order' => 6,
+            'file_path' => storage_path('csv/area-codes.csv'),
         ],
 
         'cities' => [
-            FILL_ORDER => 1,
-            FILE_PATH => storage_path('csv/zip-codes.csv'),
+            'fill_order' => 1,
+            'file_path' => storage_path('csv/zip-codes.csv'),
         ],
 
         'countries' => [
-            FILL_ORDER => 4,
-            FILE_PATH => storage_path('csv/countries.csv'),
+            'fill_order' => 4,
+            'file_path' => storage_path('csv/countries.csv'),
         ],
 
         'locations' => [
-            FILL_ORDER => 5,
-            FILE_PATH => storage_path('csv/zip-codes.csv'),
+            'fill_order' => 5,
+            'file_path' => storage_path('csv/zip-codes.csv'),
         ],
 
         'states' => [
-            FILL_ORDER => 2,
-            FILE_PATH => storage_path('csv/states.csv'),
+            'fill_order' => 2,
+            'file_path' => storage_path('csv/states.csv'),
         ],
 
         'zip_codes' => [
-            FILL_ORDER => 3,
-            FILE_PATH => storage_path('csv/zip-codes.csv'),
+            'fill_order' => 3,
+            'file_path' => storage_path('csv/zip-codes.csv'),
         ],
 
     ],
