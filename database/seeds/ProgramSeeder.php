24d34285f55f0e93621dac017e81769b202c3ecd
Kyle Anderson <Kyle@divspace.com>
2020-06-12 04:00:22 -0700

    Seed the `program_types` table

--- database/seeds/ProgramSeeder.php
+++ database/seeds/ProgramSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Database\LookupSeeder;
 use App\Database\Models\Program;
+use App\Database\Models\ProgramType;
 
 final class ProgramSeeder extends LookupSeeder
 {
@@ -10,9 +11,15 @@ final class ProgramSeeder extends LookupSeeder
         (new Lookup(Program::TABLE))
             ->fetch()
             ->each(function (array $programs, string $type): void {
+                $programTypeId = DB::table(ProgramType::TABLE)->insertGetId([
+                    ProgramType::NAME => Str::of($type)->singular(),
+                    ProgramType::CREATED_AT => $this->timestamp,
+                    ProgramType::UPDATED_AT => $this->timestamp,
+                ]);
+
                 foreach ($programs as $program) {
                     $this->insertData->push([
-                        Program::TYPE => $type === 'recovery_programs' ? 1 : 2,
+                        Program::TYPE => $programTypeId,
                         Program::NAME => $program[Program::NAME],
                         Program::ABBREVIATION => $program[Program::ABBREVIATION] ?? null,
                         Program::CREATED_AT => $this->timestamp,
