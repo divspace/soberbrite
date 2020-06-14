192bafe1e384b516ae596f15b45c0b8723ed6822
Kyle Anderson <Kyle@divspace.com>
2020-06-14 14:13:32 -0700

    Add Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,14 @@
 const mix = require('laravel-mix');
+const tailwind = require('tailwindcss');
 
 mix
     .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            tailwind('./tailwind.config.js'),
+        ],
+    })
+    .version()
     .disableNotifications();
-
-if (mix.inProduction()) {
-    mix.version();
-}
